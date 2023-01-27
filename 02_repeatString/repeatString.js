9d729a8d5da4404d39b2efc84746b821fd473208
itsjordanmuller <itsjordanmuller@gmail.com>
2023-01-27 13:31:14 -0800

    7/7 tests completed with exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num<0) return "ERROR";
+    return string.repeat(num);
 };
+console.log(repeatString("string", -10));
 
 // Do not edit below this line
 module.exports = repeatString;
