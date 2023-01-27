7a81b193607f1534dd7bf650f360bfa02b979d5c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-01-27 13:54:07 -0800

    7/7 tests passed per exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(array, ...itemsToRemove) {
+    return array.filter(item => itemsToRemove.indexOf(item) === -1);
+}
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
