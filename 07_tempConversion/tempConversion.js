864d85eeda2526c7e3b1926019fedc3ceacece14
itsjordanmuller <itsjordanmuller@gmail.com>
2023-01-28 00:11:21 -0800

    Passed 6/6 tests per exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit_temp) {
+  return Math.round((fahrenheit_temp - 32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius_temp) {
+  return Math.round((celsius_temp * 9/5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
