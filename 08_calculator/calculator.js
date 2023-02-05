9483a995123af53404a87c2665f58a4504721282
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-04 19:23:24 -0800

    16/16 tests passed per TOP JS Part 5 Exercise 1

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let total = 0;
+	for (let i = 0; i < nums.length; i++) {
+		total += nums[i];
+	}
+	return total;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+	let total = nums[0];
+	for (let i = 1; i < nums.length; i++) {
+		total *= nums[i];
+	}
+	return total;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	let total = 1;
+	for (let i = 0; i < exp; i++) {
+		total *= num;
+	}
+	return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+	for (let i = 1; i <= num; i++) {
+		total *= i;
+	}
+	return total;
 };
 
 // Do not edit below this line
