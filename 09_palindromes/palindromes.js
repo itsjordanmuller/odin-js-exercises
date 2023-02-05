3dd1cf7270336bb99dd9221e6e540a72b17de80c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-04 19:29:19 -0800

    6/6 tests passed per TOP JS Part 5 Exercise 2

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //Lowercase the string and use the RegExp to remove unwanted characters
+    var reg = str.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
 
-};
+    //Check whether the string is empty
+    if (reg.length === 0) return false;
+
+    //Reverse the string
+    var reversed = reg.split('').reverse().join('');
+
+    //Check if reversed is the same as the original
+    return reg === reversed;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
