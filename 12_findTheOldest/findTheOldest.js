0289987e9f160935de2b35dc7fb6b7a7c550536e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-04 19:45:50 -0800

    3/3 tests passed per TOP JS Part 5 Exercise 5

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+      const oldestAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : new Date().getFullYear() - oldest.yearOfBirth;
+      const currentAge = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth;
+      return oldestAge > currentAge ? oldest : person;
+    });
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
