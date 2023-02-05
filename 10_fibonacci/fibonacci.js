3287924607547db30509d9a01084d2f0e57c0295
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-04 19:40:13 -0800

    9/9 tests passed per TOP JS Part 5 Exercise 3

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n !== 'number' || n < 0) {
+      if (typeof n === 'string' && !isNaN(Number(n))) {
+        n = Number(n);
+      } else {
+        return 'OOPS';
+      }
+    }
 
+    let first = 0;
+    let second = 1;
+    let temp;
+
+    for (let i = 0; i < n; i += 1) {
+      temp = first;
+      first = second;
+      second = temp + second;
+    }
+
+    return first;
 };
+  
+console.log(fibonacci(4)); // returns 3
+console.log(fibonacci(6)); // returns 8
+console.log(fibonacci(-1)); // returns 'OOPS'
+console.log(fibonacci('string')); // returns 'OOPS'
+console.log(fibonacci('1')); // returns 1
 
 // Do not edit below this line
 module.exports = fibonacci;
